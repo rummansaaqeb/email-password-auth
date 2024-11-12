a317d96fec268460fabc155cce69516e818e0675
rummansaaqeb <rsaaqeb@gmail.com>
2024-11-12 17:29:50 +0600

    project setup react rotuer, tailwind and daisyUI

---
+++ src/__auth__.js
@@ -0,0 +1,7 @@
+/**
+ * 1. Login, Sign In ==> You are an old user , and you already have the credentials (email and password) to login.
+ * 
+ * 2. register === sign up ==> create an account ==> You are a brand new user. You have never created an account in this website. you do not have any email and password that have access.
+ * 
+ * 3.  
+ */
